40c418d16fe1d497144fa7a1aef2a8c7812db1ff
amt139 <atelich@gmail.com>
2018-02-14 18:58:35 -0500

    Astar working without look ahead

--- src/AstarNode.java
+++ src/AstarNode.java
@@ -1,5 +1,4 @@
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * The AstarNode Class
@@ -11,57 +10,46 @@ public class AstarNode {
     // x and y coordinates of the node
     private final int x, y;
     private final AstarAgent.MapLocation mapLocation;
-    
     // 2D array for the map
     private int[][] generatedMap;
-    
     // The GameMap object
     private GameMap gameMap;
-
-    // The AstarNode which this one came from
-    private final AstarNode cameFrom;
-    
     // Stores the heuristic for the node if necessary
     private Double cachedHeuristic;
     
     /**
      * Constructor declaration for AstarNode
      *
-     * @param x An int for the x-coordinate of the node
-     * @param y An int for the y-coordinate of the node
+     * @param x       An int for the x-coordinate of the node
+     * @param y       An int for the y-coordinate of the node
      * @param gameMap A HashMap for the map, where the Set of Integers is the y-values and
-     *            each y-value corresponds to an ArrayList of Integers for x-values in
-     *            that row.
+     *                each y-value corresponds to an ArrayList of Integers for x-values in
+     *                that row.
      *
      * @author Alexander Telich
      */
-    public AstarNode(int x, int y, GameMap gameMap,
-                     AstarNode cameFrom) {
+    public AstarNode(int x, int y, GameMap gameMap) {
         this.mapLocation = new AstarAgent.MapLocation(x, y, null, 0);
-        this.x = mapLocation.x;
-        this.y = mapLocation.y;
+        this.x = x;
+        this.y = y;
         this.generatedMap = gameMap.getGeneratedMap();
         this.gameMap = gameMap;
-        this.cameFrom = cameFrom;
     }
-
+    
     /**
      * Method getHeuristic calculates the heuristic
      *
      * @param goal The goal of the Astar search
+     *
      * @return A double that is the calculated heuristic to the goal
      *
      * @author Alexander Telich
      */
     public Double getHeuristic(AstarAgent.MapLocation goal) {
         // Using Chebyshev distance formula provided in the assignment instructions
-        double heuristic = Math.max(Math.abs(goal.x - x), Math.abs(goal.y - y));
-        
-        
-        
-        return heuristic;
+        return (double) Math.max(Math.abs(goal.x - x), Math.abs(goal.y - y));
     }
-
+    
     /**
      * Method getNeighbors gets the AstarNode that are neighbors of the current AstarNode
      *
@@ -72,41 +60,32 @@ public class AstarNode {
      */
     public LinkedList<AstarNode> getNeighbors() {
         LinkedList<AstarNode> neighbors = new LinkedList<>();
-
-        for(int i = x - 1; i <= x + 1; i++) {
-            for(int j = y - 1; j <= y + 1; j++) {
-                if((i == x && j == y) || i < 0 || j < 0 || j >= gameMap.getLengthY() ||
-                   i >= gameMap.getLengthX()){
+        
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                if ((i == x && j == y) || i < 0 || j < 0 || j >= gameMap.getLengthY() ||
+                    i >= gameMap.getLengthX()) {
                     continue;
                 }
                 
-                if(generatedMap[i][j] == 0) {
-                    neighbors.add(new AstarNode(i, j, gameMap, this));
+                if (generatedMap[i][j] == 0) {
+                    neighbors.add(new AstarNode(i, j, gameMap));
                 }
             }
         }
         return neighbors;
     }
-
+    
     /**
-     * Method getTraverseCost gets the cost of traversing from this node to a neighbor node
+     * Method getTraverseCost gets the cost of traversing from this node to a neighbor
+     * node
+     *
+     * @return cost of traversing to a neighbor is always 1
      *
      * @author Daniel Luo
-     * @return cost of traversing to a neighbor is always 1
      */
     public int getTraverseCost() {
         return 1;
-    }
-
-    /**
-     * Method getCameFrom gets the AstarNode that preceded the current AstarNode in the
-     * agent's path
-     *
-     * @author Daniel Luo
-     * @return previous AstarNode in path
-     */
-    public AstarNode getCameFrom() {
-        return cameFrom;
     }
     
     /**
@@ -119,6 +98,7 @@ public class AstarNode {
     public int getX() {
         return x;
     }
+    
     public int getY() {
         return y;
     }
@@ -151,9 +131,6 @@ public class AstarNode {
     public void setCachedHeuristic(Double cachedHeuristic) {
         this.cachedHeuristic = cachedHeuristic;
     }
-    
-    
-    
     
     
 }
--- src/testUpdateEnemyLocation.java
+++ src/testUpdateEnemyLocation.java
@@ -1,80 +1,80 @@
-
-/**
- * Testing class for updateEnemyLocationMethod
- *
- * @author Daniel Luo
- */
-public class testUpdateEnemyLocation {
-    /**
-     * Method prints string "board" format
-     */
-    public static String arrayToString(Integer[][] arr) {
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print('[');
-            sb.append('[');
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[j][i] + "  ");
-                sb.append(arr[j][i] + "  ");
-
-            }
-            System.out.print("]\n");
-            sb.append("]\n");
-        }
-        System.out.println("");
-
-        return sb.toString();
-
-    }
-
-    /**
-     * main method to put testing methods in
-     * @param args holds the main method arguments
-     */
-    public static void main(String[] args) {
-        Integer[][] map = new Integer[10][10];
-
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; j++) {
-                map[i][j] = 0;
-            }
-        }
-
-        // places trees at following locations
-        map[2][6] = 1;
-        map[2][8] = 1;
-        map[4][2] = 1;
-        map[6][7] = 1;
-        map[6][8] = 1;
-        map[7][0] = 1;
-        map[9][2] = 1;
-        map[9][4] = 1;
-        map[9][6] = 1;
-
-        Integer[][] updatedMap = new Integer[10][10];
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; j++) {
-                updatedMap[i][j] = map[i][j];
-            }
-        }
-
-        updatedMap[0][1] = 2;
-
-        // creates MapLocation of an enemy
-        AstarAgent.MapLocation enemyLoc = new AstarAgent.MapLocation(0,1,null,0);
-
-        AstarAgent.GameMap gameMap = new AstarAgent.GameMap(10,10,enemyLoc,null,new AstarAgent.MapLocation(0,0,null,0));
-
-        gameMap.setGeneratedMap(map);
-        gameMap.updateEnemyLocation(enemyLoc);
-
-        Integer[][] a = gameMap.getGeneratedMap();
-
-        if(testUpdateEnemyLocation.arrayToString(updatedMap).equals(testUpdateEnemyLocation.arrayToString(a)))
-            System.out.println("TRUE TEST PASSED");
-        else
-            System.out.println("FALSE TEST FAILED");
-    }
-
-}
+//
+///**
+// * Testing class for updateEnemyLocationMethod
+// *
+// * @author Daniel Luo
+// */
+//public class testUpdateEnemyLocation {
+//    /**
+//     * Method prints string "board" format
+//     */
+//    public static String arrayToString(Integer[][] arr) {
+//        StringBuilder sb = new StringBuilder();
+//
+//        for(int i = 0; i < arr.length; i++) {
+//            System.out.print('[');
+//            sb.append('[');
+//            for (int j = 0; j < arr[i].length; j++) {
+//                System.out.print(arr[j][i] + "  ");
+//                sb.append(arr[j][i] + "  ");
+//
+//            }
+//            System.out.print("]\n");
+//            sb.append("]\n");
+//        }
+//        System.out.println("");
+//
+//        return sb.toString();
+//
+//    }
+//
+//    /**
+//     * main method to put testing methods in
+//     * @param args holds the main method arguments
+//     */
+//    public static void main(String[] args) {
+//        Integer[][] map = new Integer[10][10];
+//
+//        for(int i = 0; i < 10; i++) {
+//            for(int j = 0; j < 10; j++) {
+//                map[i][j] = 0;
+//            }
+//        }
+//
+//        // places trees at following locations
+//        map[2][6] = 1;
+//        map[2][8] = 1;
+//        map[4][2] = 1;
+//        map[6][7] = 1;
+//        map[6][8] = 1;
+//        map[7][0] = 1;
+//        map[9][2] = 1;
+//        map[9][4] = 1;
+//        map[9][6] = 1;
+//
+//        Integer[][] updatedMap = new Integer[10][10];
+//        for(int i = 0; i < 10; i++) {
+//            for(int j = 0; j < 10; j++) {
+//                updatedMap[i][j] = map[i][j];
+//            }
+//        }
+//
+//        updatedMap[0][1] = 2;
+//
+//        // creates MapLocation of an enemy
+//        AstarAgent.MapLocation enemyLoc = new AstarAgent.MapLocation(0,1,null,0);
+//
+//        AstarAgent.GameMap gameMap = new AstarAgent.GameMap(10,10,enemyLoc,null,new AstarAgent.MapLocation(0,0,null,0));
+//
+//        gameMap.setGeneratedMap(map);
+//        gameMap.updateEnemyLocation(enemyLoc);
+//
+//        Integer[][] a = gameMap.getGeneratedMap();
+//
+//        if(testUpdateEnemyLocation.arrayToString(updatedMap).equals(testUpdateEnemyLocation.arrayToString(a)))
+//            System.out.println("TRUE TEST PASSED");
+//        else
+//            System.out.println("FALSE TEST FAILED");
+//    }
+//
+//}
